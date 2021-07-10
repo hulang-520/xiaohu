f646fa80fbdf9055306d42d85bd9ac7a8a4abe61
hulang-520 <64276957+hulang-520@users.noreply.github.com>
2021-07-10 23:12:10 +0800

    Add files via upload

--- jf_func.js
+++ jf_func.js
@@ -2,6 +2,6 @@ var objFunc = require('./jf_sign')
 console.log('本程序仅供交流学习使用，请勿使用本程序进行商业行为。');
 console.log('您应该在下载后的24小时内从您的计算机中删除本程序。');
 console.log('由使用本程序所产生的一切责任自负。');
-console.log('by yaohuo 30235');
-var para="b568f5a9*****bf8"
+console.log('by yaohuo xiaohu');
+var para="b489f5a9e5a28c719e1bd33cab03ea6e40fa13b0a3dd2d96ec943747a6dadb941fb6acce6ed360bd37c38955290313462591a4b1858195eca8bacebf737d1abeff8956bffa33409f8fe9d178f6db6b08"
 objFunc.func()
